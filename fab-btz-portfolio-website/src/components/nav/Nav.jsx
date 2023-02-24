d7245b64349a501be054e6e81f878ebdce9dadb4
Fab-Btz-Dev <fabianbenitez_87@hotmail.com>
2023-02-24 14:56:15 -0300

    nav

--- fab-btz-portfolio-website/src/components/nav/Nav.jsx
+++ fab-btz-portfolio-website/src/components/nav/Nav.jsx
@@ -7,12 +7,15 @@ import {MdOutlineHomeRepairService} from 'react-icons/md'
 import {SlBriefcase} from 'react-icons/sl'
 import {BiMessageDetail} from 'react-icons/bi'
 
+import { useState } from 'react'
+
 const Nav = () => {
+  const {activeNav, setActiveNav} = useState('#header')
   return (
     <nav>
-      <a href="#header"><GoHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#experience"><BsBookmarkStar /></a>
+      <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}><GoHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" className='active'><BsBookmarkStar /></a>
       <a href="#services"><MdOutlineHomeRepairService /></a>
       <a href="#portfolio"><SlBriefcase /></a>
       <a href="#contact"><BiMessageDetail /></a>
